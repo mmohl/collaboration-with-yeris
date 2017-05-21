0aa4d452bdccd4a3cb925d4a2739008a4956276e
mmohl <mmohammadl.lpkia@gmail.com>
2017-05-21 08:49:19 +0700

    ubah menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,8 +30,11 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Teknik', 'url'=>array('/teknik/index')),
+// 				array('label'=>'Prediksi', 'url'=>array('/prediksi/index')),
+				array('label'=>'Import', 'url'=>array('/data/import')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
